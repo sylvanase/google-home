c72d36b9bbfa0bd86e21b4528877f9c68b3b495c
sylvanas <jzm362466464@126.com>
2021-02-13 19:01:19 +0800

    initpro

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,116 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import React from 'react'
+import { Input, Row, Col, Modal, Form } from 'antd'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const iconList = [
+	{
+		icon: '/icon1.png',
+		title: '语雀',
+		link: 'https://www.yuque.com/dashboard',
+	},
+	{
+		icon: '/icon2.png',
+		title: 'npm',
+		link: 'https://www.npmjs.com/',
+	},
+	{
+		icon: '/icon4.png',
+		title: 'git',
+		link: 'https://github.com/',
+	},
+]
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+class Home extends React.Component {
+	state = {
+		showModal: false,
+	}
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+	addLink = (e) => {
+		e.stopPropagation()
+		this.setState({
+			showModal: true,
+		})
+	}
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+	handleOk = () => {
+		this.setState({
+			showModal: false,
+		})
+	}
+	handleCancel = () => {
+		this.setState({
+			showModal: false,
+		})
+	}
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+	render() {
+		const { showModal } = this.state
+		return (
+			<div className={styles.container}>
+				<Head>
+					<title>Google page</title>
+					<link rel="icon" href="/favicon.ico" />
+				</Head>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+				<main className={styles.main}>
+					<h1 className={styles.title}>
+						<img src="/google_logo.svg" alt="logo" />
+					</h1>
+					<Input
+						className={styles.input}
+						size="large"
+						prefix={<img src="/search.svg" className={styles.inputIcon} />}
+						suffix={
+							<img src="/googlemic_clr_24px.svg" className={styles.inputIcon} />
+						}
+						placeholder="在Google上搜索，或者输入一个网址"
+					></Input>
+					{/* icons */}
+					<Row className={styles.iconList}>
+						{iconList.map((item) => {
+							return (
+								<Col span={6} className={styles.iconItem} key={item.icon}>
+									<a href={item.link} target="_blank">
+										<div className={styles.itemIconWrap}>
+											<img src={item.icon} className={styles.itemIconSrc} />
+										</div>
+										<div className={styles.iconLink}>{item.title}</div>
+									</a>
+								</Col>
+							)
+						})}
+						<Col span={6} className={styles.iconItem}>
+							<a onClick={(e) => this.addLink(e)}>
+								<div className={styles.itemIconWrap}>
+									<img src="/add.svg" className={styles.itemIconSrc} />
+								</div>
+								<div className={styles.iconLink}>添加快捷方式</div>
+							</a>
+						</Col>
+					</Row>
+				</main>
+				<Modal
+					title="添加快捷方式"
+					visible={showModal}
+					onOk={() => this.handleOk()}
+					onCancel={() => this.handleCancel()}
+					okText="完成"
+					cancelText="取消"
+					destroyOnClose={true}
+				>
+					<Form preserve={false} layout="vertical">
+						<Form.Item label="名称" name="">
+							<Input placeholder="" />
+						</Form.Item>
+						<Form.Item label="网址" name="">
+							<Input placeholder="" />
+						</Form.Item>
+					</Form>
+				</Modal>
+			</div>
+		)
+	}
 }
+
+export default Home
